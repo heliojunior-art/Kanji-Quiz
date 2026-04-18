7e9634012540cf17dfebc0f8afd3bb4ab84ef760
HELIO <heliojunior@eduunifil.br>
2026-04-18 12:44:00 -0300

    add migrations

--- QuizKanji/settings.py
+++ QuizKanji/settings.py
@@ -1,5 +1,4 @@
 from pathlib import Path
-import dj_database_url
 import os
 
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -52,9 +51,14 @@ WSGI_APPLICATION = 'QuizKanji.wsgi.application'
 # MySQL
 # ------------------------------
 DATABASES = {
-        'default': dj_database_url.config(
-            default='sqlite:///db.sqlite3'
-    )
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+        'USER': 'root',
+        'PASSWORD': '3352',
+        'HOST': 'localhost',
+        'PORT': '3306',
+    }
 }
 
 AUTH_PASSWORD_VALIDATORS = [
